7a4d5add9c4e9b514cbd970be6c9709fe0657223
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2017-01-28 08:23:50 +0100

    Sync

---
+++ src/todosVue.js
@@ -0,0 +1,9 @@
+const todosVue = {
+  STORAGE_TOKEN_KEY: 'token',
+  OAUTH_CLIENT_ID: 5,
+  OAUTH_REDIRECT_URI: 'http://localhost:8095/login',
+  OAUTH_SERVER_URL: 'http://todos.dev:8080/oauth/authorize?',
+  API_TASK_URL: 'http://todos.dev:8080/api/v1/task',
+  API_PROFILE_URL: 'http://todos.dev:8080/api/v1/user'
+}
+export default todosVue
